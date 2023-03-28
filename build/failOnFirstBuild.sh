5a3c993a4f8415a1a615145aed83cf0b02b2ff5b
Kristian Dorland <kristian@dorland.dk>
2023-03-28 12:25:50 +0200

    Efter setup.sh-kørsel

--- build/failOnFirstBuild.sh
+++ build/failOnFirstBuild.sh
@@ -2,7 +2,7 @@
 
 echo "${GITHUB_REPOSITORY}"
 echo "${DOCKER_SERVICE}"
-if [ "${GITHUB_REPOSITORY}" != "kvalitetsit/kittyact" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/kittyact" ]; then
+if [ "${GITHUB_REPOSITORY}" != "kvalitetsit/fut-patient-web" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/fut-patient-web" ]; then
   echo "Please run setup.sh REPOSITORY_NAME"
   exit 1
 fi
